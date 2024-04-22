808456768977a7e1955f52a93d4924d16a07da06
Ramshankar-Bhuvaneswaran <bhuvaneshwaran.r@northeastern.edu>
2024-04-22 18:30:03 -0400

    Adding Agent definition and tasks

--- app.py
+++ app.py
@@ -30,4 +30,61 @@ def main():
         model=model,
         temperature=temprature
     )
+    tasks = ProjectTasks()
+    agents = ProjectAgents()
+    
+    # Create agents
+    Problem_Definition_Agent = agents.Problem_Definition_Agent(model=llm)
+    Data_Assessment_Agent = agents.Data_Assessment_Agent(model=llm)
+    Model_Recommendation_Agent = agents.Model_Recommendation_Agent(model=llm)
+    Starter_Code_Generator_Agent = agents.Starter_Code_Generator_Agent(model=llm)
+    
+    # User input
+    user_question = st.text_area("Describe your ML Doubt:")
+    st.info("Please select the steps you would like to include in the workflow along with Starting Code:")
+    task1 = st.checkbox("Problem Definition")
+    task2 = st.checkbox("Data Assessment")
+    task3 = st.checkbox("Model Recommendation")
+    submit_button = st.button("Submit")
+    data_upload = False    
+            
+# Building the CrewAI workflow
+    if submit_button and user_question:
+        
+        task_define_problem = tasks.task_define_problem(agent=Problem_Definition_Agent, user_question=user_question)
+        
+        # Choose the appropriate task based on whether data was uploaded
+        if data_upload:
+            task_assess_data = tasks.task_assess_data_1(agent=Data_Assessment_Agent, df=df, uploaded_file=uploaded_file.name)
+        else:
+            task_assess_data = tasks.task_access_data_2(agent=Data_Assessment_Agent)
+            
+        task_recommend_model = tasks.task_recommend_model(agent=Model_Recommendation_Agent)
+        
+        task_generate_code = tasks.task_generate_code(agent=Starter_Code_Generator_Agent)
+
+        
+        crew = Crew(
+            agents=[Problem_Definition_Agent, Data_Assessment_Agent, Model_Recommendation_Agent, Starter_Code_Generator_Agent],
+            tasks=[task_define_problem, task_assess_data, task_recommend_model,  task_generate_code],
+            verbose=2
+        )
+        
+        # Run the CrewAI workflow
+        result = crew.kickoff()
+        
+        # Display the final output
+        if task1:
+            st.chat_message(name="ai", avatar="🤖").write(task_define_problem.output.result)
+        if task2:
+            st.chat_message(name="ai", avatar="🤖").write(task_assess_data.output.result)
+        if task3:
+            st.chat_message(name="ai", avatar="🤖").write(task_recommend_model.output.result)
+        st.chat_message(name="ai", avatar="🤖").write(result)
+    
+
+if __name__ == '__main__':
+    main()
+
+
     
