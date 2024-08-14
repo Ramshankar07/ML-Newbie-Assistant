a398f1359dedd7670c654266a3abac7e18dbe86f
Ramshankar <87872158+Ramshankar07@users.noreply.github.com>
2024-08-14 22:10:51 +0530

    present

--- app.py
+++ app.py
@@ -6,87 +6,71 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from tasks import ProjectTasks
 from agents import ProjectAgents
+from groq import Groq
+import random
 
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import PromptTemplate
 load_dotenv()
 groq_api_key = st.secrets['GROQ_API_KEY']
 
 def main():
-    # Setting the title and description 
-    st.title("Machine Learning Newbie Assistant")
-    multiline_text = """
-    This Machine Learning Assistant is designed to guide users through the process of defining, assessing, and solving machine learning problems. It leverages a team of AI agents, each with a specific role, to clarify the problem, evaluate the data, recommend suitable models, and generate starter Python code. 
     """
-    st.markdown(multiline_text, unsafe_allow_html=True)
+    This function is the main entry point of the application. It sets up the Groq client, the Streamlit interface, and handles the chat interaction.
+    """
     
-    # Sidebar customization for models
+    # Get Groq API key
+    groq_api_key = os.environ['GROQ_API_KEY']
+
+    # Display the Groq logo
+    spacer, col = st.columns([5, 1])  
+    with col:  
+        st.image('groqcloud_darkmode.png')
+
+    # The title and greeting message of the Streamlit application
+    st.title("Chat with Groq!")
+    st.write("Hello! I'm your friendly Groq chatbot. I can help answer your questions, provide information, or just chat. I'm also super fast! Let's start our conversation!")
+
+    # Add customization options to the sidebar
     st.sidebar.title('Customization')
     model = st.sidebar.selectbox(
-        'Choose a Model',
+        'Choose a model',
         ['mixtral-8x7b-32768', 'llama2-70b-4096']
     )
-    temprature = st.sidebar.slider('Model Temprature', 0.0, 1.0, 0.0)
-    
-    # Initialize the ChatGroq model
-    llm = ChatGroq(
-        api_key=groq_api_key,
-        model=model,
-        temperature=temprature
+    conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
+
+    memory=ConversationBufferWindowMemory(k=conversational_memory_length)
+
+    user_question = st.text_input("Ask a question:")
+
+    # session state variable
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history=[]
+    else:
+        for message in st.session_state.chat_history:
+            memory.save_context({'input':message['human']},{'output':message['AI']})
+
+
+    # Initialize Groq Langchain chat object and conversation
+    groq_chat = ChatGroq(
+            groq_api_key=groq_api_key, 
+            model_name=model
     )
-    tasks = ProjectTasks()
-    agents = ProjectAgents()
-    
-    # Create agents
-    Problem_Definition_Agent = agents.Problem_Definition_Agent(model=llm)
-    Data_Assessment_Agent = agents.Data_Assessment_Agent(model=llm)
-    Model_Recommendation_Agent = agents.Model_Recommendation_Agent(model=llm)
-    Starter_Code_Generator_Agent = agents.Starter_Code_Generator_Agent(model=llm)
-    
-    # User input
-    user_question = st.text_area("Describe your ML Doubt:")
-    st.info("Please select the steps you would like to include in the workflow along with Starting Code:")
-    task1 = st.checkbox("Problem Definition")
-    task2 = st.checkbox("Data Assessment")
-    task3 = st.checkbox("Model Recommendation")
-    submit_button = st.button("Submit")
-    data_upload = False    
-            
-# Building the CrewAI workflow
-    if submit_button and user_question:
-        
-        task_define_problem = tasks.task_define_problem(agent=Problem_Definition_Agent, user_question=user_question)
-        
-        # Choose the appropriate task based on whether data was uploaded
-        if data_upload:
-            task_assess_data = tasks.task_assess_data_1(agent=Data_Assessment_Agent, df=df, uploaded_file=uploaded_file.name)
-        else:
-            task_assess_data = tasks.task_access_data_2(agent=Data_Assessment_Agent)
-            
-        task_recommend_model = tasks.task_recommend_model(agent=Model_Recommendation_Agent)
-        
-        task_generate_code = tasks.task_generate_code(agent=Starter_Code_Generator_Agent)
 
-        
-        crew = Crew(
-            agents=[Problem_Definition_Agent, Data_Assessment_Agent, Model_Recommendation_Agent, Starter_Code_Generator_Agent],
-            tasks=[task_define_problem, task_assess_data, task_recommend_model,  task_generate_code],
-            verbose=2
-        )
-        
-        # Run the CrewAI workflow
-        result = crew.kickoff()
-        
-        # Display the final output
-        if task1:
-            st.chat_message(name="ai", avatar="🤖").write(task_define_problem.output.result)
-        if task2:
-            st.chat_message(name="ai", avatar="🤖").write(task_assess_data.output.result)
-        if task3:
-            st.chat_message(name="ai", avatar="🤖").write(task_recommend_model.output.result)
-        st.chat_message(name="ai", avatar="🤖").write(result)
-    
+    conversation = ConversationChain(
+            llm=groq_chat,
+            memory=memory
+    )
 
-if __name__ == '__main__':
+    # If the user has asked a question,
+    if user_question:
+        
+        # The chatbot's answer is generated by sending the full prompt to the Groq API.
+        response = conversation(user_question)
+        message = {'human':user_question,'AI':response['response']}
+        st.session_state.chat_history.append(message)
+        st.write("Chatbot:", response['response'])
+
+if __name__ == "__main__":
     main()
-
-
-    
